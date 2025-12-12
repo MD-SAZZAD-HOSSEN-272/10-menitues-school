8d1c054b3bb1139cfcaaab8032b6cd8953d9c018
MD-SAZZAD-HOSSEN-272 <md.sazzadhosen325@gmail.com>
2025-12-12 21:36:02 +0600

    first commit

---
+++ src/app/Components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import image1 from '../'
+
+const HeroSection = () => {
+    return (
+        <div className='bg-linear-to-r from-[#9D0B0B] to-[#D25C09] py-10'>
+            <h2 className='text-4xl text-center  text-white'>শেখা হোক আনন্দে!</h2>\
+            <h5 className='text-xl text-center text-white'>এক জায়গায় স্কুল ও কলেজের সম্পূর্ণ প্রস্তুতি!</h5>
+            <div>
+                <div className="on-admission">
+                    <img src="" alt="" />
+                </div>
+                <div className="on-admission"></div>
+                <div className="on-admission"></div>
+                <div className="on-admission"></div>
+                <div className="on-admission"></div>
+                <div className="on-admission"></div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
